625a956b9baea3f46ac40fdfe136c90857e13b51
Amelia Maier <ame.m2805@gmail.com>
2026-04-30 16:45:46 +0200

    Included dependency springframework.boot

--- backend/src/main/java/org/example/backend/controller/IndexController.java
+++ backend/src/main/java/org/example/backend/controller/IndexController.java
@@ -1,6 +1,5 @@
 package org.example.backend.controller;
 
-import org.example.backend.dto.TodoDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
